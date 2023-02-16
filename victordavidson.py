3e35c836979b988f186dafdabc5e6d381599c6b1
s1338289 <124280805+s1338289@users.noreply.github.com>
2023-02-16 10:33:36 -0500

    Add files via upload
    trying collaboration

---
+++ victordavidson.py
@@ -0,0 +1,8 @@
+
+howMany = 3
+howManyEntered = 2
+if howManyEntered < howMany:
+    test_score = int(input("enter score: "))
+    howManyEntered  = howManyEntered + 1
+else:
+    print("I am done")
